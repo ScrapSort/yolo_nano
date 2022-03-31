9722fec53af0397ba803e3d670eb57f93fef75aa
satish1901 <satishkumar@ucsb.edu>
2022-03-31 16:16:23 -0700

    cleaning

--- train_cus.sh
+++ train_cus.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #CUDA_LAUNCH_BLOCKING=1 python train.py --model_def ./config/yolo-nano_cus.cfg --data_config config/custom.data --batch_size 2 --epochs 10
-python train.py --model_def ./config/yolo-nano_cus.cfg --data_config config/custom.data --batch_size 24 --epochs 200
+#python train.py --model_def ./config/yolo-nano_cus.cfg --data_config config/custom.data --batch_size 8 --epochs 200
+CUDA_VISIBLE_DEVICES=0 python train.py --model_def ./config/yolo-nano_cus.cfg --data_config config/custom.data --batch_size 8 --epochs 200
 
